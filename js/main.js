6490dba2b3da430dd5afea39844c5b1084ba01cf
tayfurparmak <74604272+tayfurparmak@users.noreply.github.com>
2022-04-06 17:34:45 +0300

    ege_üniversitesi_gençlik_spor_külübünün_websitesi

---
+++ js/main.js
@@ -0,0 +1,250 @@
+(function ($) {
+    "use strict";
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $('.nav-bar').addClass('nav-sticky');
+        } else {
+            $('.nav-bar').removeClass('nav-sticky');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 768) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Top News Slider
+    $('.tn-slider').slick({
+        autoplay: true,
+        infinite: true,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    
+    
+    // Category News Slider
+    $('.cn-slider').slick({
+        autoplay: false,
+        infinite: true,
+        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    
+    
+    // Related News Slider
+    $('.sn-slider').slick({
+        autoplay: false,
+        infinite: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+})(jQuery);
+
+$(function() {
+    function c() {
+        p();
+        var e = h();
+        var r = 0;
+        var u = false;
+        l.empty();
+        while (!u) {
+            if (s[r] == e[0].weekday) {
+                u = true
+            } else {
+                l.append('<div class="blank"></div>');
+                r++
+            }
+        }
+        for (var c = 0; c < 42 - r; c++) {
+            if (c >= e.length) {
+                l.append('<div class="blank"></div>')
+            } else {
+                var v = e[c].day;
+                var m = g(new Date(t, n - 1, v)) ? '<div class="today">' : "<div>";
+                l.append(m + "" + v + "</div>")
+            }
+        }
+        var y = o[n - 1];
+        a.css("background-color", y).find("h1").text(i[n - 1] + " " + t);
+        f.find("div").css("color", y);
+        l.find(".today").css("background-color", y);
+        d()
+    }
+
+    function h() {
+        var e = [];
+        for (var r = 1; r < v(t, n) + 1; r++) {
+            e.push({
+                day: r,
+                weekday: s[m(t, n, r)]
+            })
+        }
+        return e
+    }
+
+    function p() {
+        f.empty();
+        for (var e = 0; e < 7; e++) {
+            f.append("<div>" + s[e].substring(0, 3) + "</div>")
+        }
+    }
+
+    function d() {
+        var t;
+        var n = $("#calendar").css("width", e + "px");
+        n.find(t = "#calendar_weekdays, #calendar_content").css("width", e + "px").find("div").css({
+            width: e / 7 + "px",
+            height: e / 7 + "px",
+            "line-height": e / 7 + "px"
+        });
+        n.find("#calendar_header").css({
+            height: e * (1 / 7) + "px"
+        }).find('i[class^="icon-chevron"]').css("line-height", e * (1 / 7) + "px")
+    }
+
+    function v(e, t) {
+        return (new Date(e, t, 0)).getDate()
+    }
+
+    function m(e, t, n) {
+        return (new Date(e, t - 1, n)).getDay()
+    }
+
+    function g(e) {
+        return y(new Date) == y(e)
+    }
+
+    function y(e) {
+        return e.getFullYear() + "/" + (e.getMonth() + 1) + "/" + e.getDate()
+    }
+
+    function b() {
+        var e = new Date;
+        t = e.getFullYear();
+        n = e.getMonth() + 1
+    }
+    var e = 480;
+    var t = 2013;
+    var n = 9;
+    var r = [];
+    var i = ["OCAK", "ŞUBAT", "MART", "NİSAN", "MAYIS", "HAZİRAN", "TEMMUZ", "AĞUSTOS", "EYLÜL", "EKİM", "KASIM", "ARALIK"];
+    var s = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "CUMAERTESİ"];
+    var o = ["#16a085", "#1abc9c", "#c0392b", "#27ae60", "#FF6860", "#f39c12", "#f1c40f", "#e67e22", "#2ecc71", "#e74c3c", "#d35400", "#2c3e50"];
+    var u = $("#calendar");
+    var a = u.find("#calendar_header");
+    var f = u.find("#calendar_weekdays");
+    var l = u.find("#calendar_content");
+    b();
+    c();
+    a.find('i[class^="icon-chevron"]').on("click", function() {
+        var e = $(this);
+        var r = function(e) {
+            n = e == "next" ? n + 1 : n - 1;
+            if (n < 1) {
+                n = 12;
+                t--
+            } else if (n > 12) {
+                n = 1;
+                t++
+            }
+            c()
+        };
+        if (e.attr("class").indexOf("left") != -1) {
+            r("previous")
+        } else {
+            r("next")
+        }
+    })
+})
+$(document).ready(function () {
+    $('.navbar-light .dmenu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
+    });
